2b6a292ea5b3318369d6d2110ccdb3208ad8a8da
BrantKijin <upper0house@gmail.com>
2024-12-27 13:21:06 +0900

    초기파일

---
+++ service/article_read/src/main/java/kuke/board/article_read/ArticleReadApplication.java
@@ -0,0 +1,11 @@
+package article_read.src.main.java.kuke.board.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReadApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApplication.class, args);
+	}
+}
---
+++ service/comment/src/main/java/kuke/board/comment/CommentArticleApplication.java
@@ -0,0 +1,11 @@
+package comment.src.main.java.kuke.board.comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentArticleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CommentArticleApplication.class, args);
+	}
+}
